8d4bda8256d7bd84df477c6f5d9bb6844bc5bc38
Rebecca <rebeccaabel@hotmail.se>
2022-12-15 14:32:00 +0100

    gamelogic about done, check message

--- app.js
+++ app.js
@@ -1,11 +1,76 @@
-const userScore = 0;
-const computerScore = 0;
+let userScore = 0;
+let computerScore = 0;
 const userScore_span = document.getElementById("user-score");
 const computerScore_span = document.getElementById("computer-score");
 const scoreBoard_div = document.querySelector(".score-board");
-const result_div = document.querySelector(".result");
-const rock_div = document.getElementById("r");
-const paper_div = document.getElementById("p");
-const scissors_div = document.getElementById("s");
+const result_p = document.querySelector(".result > p");
+const rock_div = document.getElementById("rock");
+const paper_div = document.getElementById("paper");
+const scissor_div = document.getElementById("scissor");
 
-rock_div.addEventListener('click',)
+
+function getComputerChoice() {
+    let choices = ['rock', 'paper', 'scissor']
+    let randomChoice = Math.floor(Math.random() * 3)
+    return choices[randomChoice];
+}
+
+function userWin(userChoice, computerChoice) {
+    userScore++;
+    userScore_span.innerHTML = userScore;
+    computerScore_span.innerHTML = computerScore; 
+    result_p.innerHTML = userChoice + "beats" + computerChoice + " You Win!!!!";
+
+
+    //console.log("WIN");
+}
+
+function userLose() {
+    console.log("LOST");
+}
+
+function draw() {
+    console.log("DRAW");
+}
+
+function game(userChoice) {
+    const computerChoice = getComputerChoice();
+    switch (userChoice + computerChoice) {
+        case 'rockscissor':
+        case "paperrock":
+        case "scissorpaper":
+            userWin();
+            break;
+        case "rockpaper":
+        case "paperscissor":
+        case "scissorrock":
+            userLose();
+            break;
+        case "rockrock":
+        case "paperpaper":
+        case "scissorscissor":
+            draw();
+            break;
+        
+    }
+    /*console.log("användare;    " + userChoice);
+    console.log("dator;        " + computerChoice);*/
+}
+
+function main() {
+    rock_div.addEventListener('click', function() {
+        game("rock")
+        //console.log("r")
+    })
+
+    paper_div.addEventListener('click', function() {
+        game("paper")
+        //console.log("p")
+    })
+
+    scissor_div.addEventListener('click', function() {
+        game("scissor")
+        //console.log("s")
+    })
+}
+main();
